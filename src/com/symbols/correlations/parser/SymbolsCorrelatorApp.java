10553307ebd5c643353176807523e37b5d7e6ce2
Valerzen88 <valeri.balachnin@gmail.com>
2018-05-14 21:56:04 +0200

    Accessing positions file from jar with lastXMonth parameter

--- src/com/symbols/correlations/parser/SymbolsCorrelatorApp.java
+++ src/com/symbols/correlations/parser/SymbolsCorrelatorApp.java
@@ -1,17 +1,29 @@
 package com.symbols.correlations.parser;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Objects;
 import java.util.Scanner;
 
 public class SymbolsCorrelatorApp {
 
     public static void main(final String[] args) throws Exception {
-        final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        final File csvFile = new File(Objects.requireNonNull(classLoader.getResource("positionsTable.csv")).getFile());
-        final Scanner scanner = new Scanner(csvFile);
-        new SymbolService(scanner, args[0].replace("lastXMonth=",""));
+        final Scanner scanner = new Scanner(getPositionsFile());
+        new SymbolService(scanner, args[0].replace("lastXMonth=", ""));
         scanner.close();
+    }
+
+    private static File getPositionsFile() {
+        final String filePathString = "positionsTable.csv";
+        final Path path = Paths.get(filePathString);
+        File positionsFile = new File(filePathString);
+        if (!Files.exists(path)) {
+            final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+            positionsFile = new File(Objects.requireNonNull(classLoader.getResource(filePathString)).getFile());
+        }
+        return positionsFile;
     }
 
 }
